7a950525bf03d4bf14c94a26357635912fc3b7ee
kennedymeadows <62924402+kennedymeadows@users.noreply.github.com>
2022-11-03 11:00:33 -0700

    testing framework and first tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # describe 'creation' do
+  #   scenario 'user can be created' do
+  #     visit new_user_registration_path
+  #     login_as(create(:user))
+  #     expect(page).to redirect_to(root_path)
+  #   end
+  # end
 end
