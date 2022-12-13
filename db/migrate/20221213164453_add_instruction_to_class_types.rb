feeabf1dbed1464aafa38c97bfd80c97e92706e9
kennedymeadows <62924402+kennedymeadows@users.noreply.github.com>
2022-12-13 09:29:53 -0800

    added instructions and edited form styling

---
+++ db/migrate/20221213164453_add_instruction_to_class_types.rb
@@ -0,0 +1,5 @@
+class AddInstructionToClassTypes < ActiveRecord::Migration[7.0]
+  def change
+    add_column :class_types, :instruction, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_190812) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_13_164453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_190812) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "gfc_id"
+    t.string "instruction"
     t.index ["gfc_id"], name: "index_class_types_on_gfc_id"
   end
 
