bff1e737cdd3e6f826051063eefd8f199aa821ac
kennedymeadows <62924402+kennedymeadows@users.noreply.github.com>
2022-12-23 12:23:23 -0800

    cleaning up some css and adding social handle

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -52,12 +52,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
   
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role, :social_handle])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role, :social_handle])
   end
 
   # The path used after sign up.
